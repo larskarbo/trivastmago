df979777bfe10b418227362d25bc89f07a64e1ae
Lars Karbo <mail@larskarbo.no>
2019-03-08 22:25:40 +0100

    jajaja

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './static/css/style.css';
+import one from './1.png'
+import two from './2.png'
+import three from './3.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="wrap">
+          <div className="inwrap">
+            <div class="yo">
+              <div class="logo">
+                <h1>
+                  <span class="red">Hot</span><span class="two">llu</span><span class="third">ft?</span>
+                </h1>
+                <h1 class="tow">
+                  <span class="red">triv</span><span class="two">astma</span><span class="third">go</span>
+                </h1>
+              </div>
+            </div>
+            <div class="yo2">
+              <img className="reveal1" src={one} />
+              <img className="reveal2" src={two} />
+              <img className="reveal3" src={three} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
